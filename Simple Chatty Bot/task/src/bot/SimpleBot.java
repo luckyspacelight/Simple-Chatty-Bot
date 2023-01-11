faf7639de44f0c0dcc83c1189c050e1db489e9a9
Luca Perti <luca.perti@gmail.com>
2023-01-11 19:52:42 +0100

    1 update

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -4,13 +4,28 @@ import java.util.Scanner;
 
 public class SimpleBot {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-        String myName = scanner.nextLine();
 
-        System.out.println("Hello! My name is Aid.");
-        System.out.println("I was created in 2018.");
+        System.out.println("Hello! My name is Universo.");
+        System.out.println("I was created in 2023.");
         System.out.println("Please, remind me your name.");
-        System.out.printf("What a great name you have, %s!", myName);
+
+        String name = scanner.nextLine();
+
+        System.out.println("What a great name you have, " + name + "!");
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+
+        final int nDigits = 3;
+        int[] numbers = new int[nDigits];
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (scanner.hasNext()) {
+                numbers[i] = scanner.nextInt();
+            }
+        }
+
+        int myAge = (numbers[0] * 70 + numbers[1] * 21 + numbers[2] * 15) % 105;
+        System.out.printf("Your age is %d; that's a good time to start programming!", myAge);
     }
 }
